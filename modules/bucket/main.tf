2355075bcd21c911e7dfd671f4a12ad4a3cc32b1
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2024-09-21 00:19:18 -0500

    agregando modulo

---
+++ modules/bucket/main.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "main" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "main" {
+  depends_on = [aws_s3_bucket_ownership_controls.main]
+
+  bucket = aws_s3_bucket.main.id
+  acl    = var.acl
+}
